e24b616de5bdcada654f7155f723f4b1164c3890
Jérôme <Jrme@benoit>
2018-02-06 00:28:51 +0100

    challah ca marche

---
+++ Entrotrain/src/core/Passenger.java
@@ -0,0 +1,35 @@
+package core;
+
+public class Passenger {
+	private int age;
+	private String sexe;
+	private Station stationOfArrival; 
+	private Station stationOfDeparture;
+	private int satisfaction;
+
+
+public Passenger(int age, String sexe, Station arrival, Station departure, int satisfaction){
+	this.age = age;
+	this.sexe = sexe;
+	this.stationOfArrival = stationOfArrival;
+	this.stationOfDeparture = stationOfDeparture;
+	this.satisfaction = satisfaction;
+}
+
+
+
+public Station getStationOfArrival() {
+	return stationOfArrival;
+}
+public void setStationOfArrival(Station stationOfArrival) {
+	this.stationOfArrival = stationOfArrival;
+}
+
+public Station getStationOfDeparture() {
+	return stationOfDeparture;
+}
+public void setStationOfDeparture(Station stationOfDeparture) {
+	this.stationOfDeparture = stationOfDeparture;
+}
+
+}
--- Entrotrain/src/core/Train.java
+++ Entrotrain/src/core/Train.java
@@ -1,6 +1,12 @@
 package core;
 
 import exception.TerminusException;
+import gui.SimulationGUI;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 
 /**
  * @author tliu@u-cergy.fr
@@ -9,6 +15,10 @@ public class Train extends Thread {
 	private volatile int position = 0;
 	private Line line;
 	private Canton currentCanton;
+	private int currentCapacity = 0;  //train leaves garage with 0 passengers
+	private int maxCapacity;
+	private Station currentStation = null ; //garage isnt a station
+	private List<Passenger> trainPassengers = new ArrayList<Passenger>();
 
 	/**
 	 * Distance per time unit.
@@ -16,10 +26,11 @@ public class Train extends Thread {
 	private int speed;
 	private boolean hasArrived = false;
 
-	public Train(Line line, Canton startCanton, int speed) {
+	public Train(Line line, Canton startCanton, int maxCapacity, int speed) {
 		this.line = line;
 		currentCanton = startCanton;
 		currentCanton.enter(this);
+		this.maxCapacity = maxCapacity;
 		this.speed = speed;
 	}
 
@@ -38,12 +49,29 @@ public class Train extends Thread {
 	public void setCurrentCanton(Canton currentCanton) {
 		this.currentCanton = currentCanton;
 	}
+	
+	public int getCurrentCapacity() {
+		return currentCapacity;
+	}
+	
+	public void setCurrentCapacity(int currentCapacity) {
+		this.currentCapacity = currentCapacity;
+	}
+
+	public int getMaxCapacity() {
+		return maxCapacity;
+	}
+
+	public void setMaxCapacity(int maxCapacity) {
+		this.maxCapacity = maxCapacity;
+	}
+
 
 	@Override
 	public void run() {
 		while (!hasArrived) {
 			try {
-				sleep(gui.SimulationGUI.TIME_UNIT);
+				sleep(SimulationGUI.TIME_UNIT);
 			} catch (InterruptedException e) {
 				System.err.println(e.getMessage());
 			}
@@ -62,6 +90,37 @@ public class Train extends Thread {
 		currentCanton.exit();
 	}
 
+	
+	/***
+	 * How to deal with the situation of a passenger arriving to his destination
+	 */
+	public void trainUnboarding() {
+		for(Iterator iter = trainPassengers.iterator(); iter.hasNext();){
+			Passenger p1 = (Passenger) iter.next();
+			if (p1.getStationOfArrival().equals(this.currentStation)) {
+				iter.remove(); //p1 got off the train and arrived at his destination
+				this.currentCapacity--;
+			}
+		}
+	}
+	
+	
+	/***
+	 * How to deal with a passenger catching his train
+	 */
+	public void trainBoarding() {
+		for(Iterator iter = this.currentStation.getPassengers().iterator(); iter.hasNext();){
+			Passenger p1 = (Passenger) iter.next();
+			if (p1.getStationOfDeparture().equals(this.currentStation) && this.currentCapacity < this.maxCapacity) {
+				iter.remove(); //p1 boards on the train and leaves the current station
+				this.currentStation.setPassenger(this.currentStation.getPassenger()-1); //Station loses 1 passenger since he boarded on the train
+				this.trainPassengers.add(p1);
+			}
+		}	
+	}
+	
+	
+	
 	@Override
 	public String toString() {
 		return "Train [speed=" + speed + "]";
