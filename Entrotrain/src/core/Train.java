66491a2a9b00a2185d13af7ced7d96aef8d73fb2
CyrilBelmonte <cyril.belmonte@hotmail.fr>
2018-02-09 13:18:39 +0100

    Ajout de la classe passager Ajout des parametres sur Train

--- Entrotrain/src/core/Train.java
+++ Entrotrain/src/core/Train.java
@@ -9,6 +9,8 @@ public class Train extends Thread {
 	private volatile int position = 0;
 	private Line line;
 	private Canton currentCanton;
+	private int maxPassenger;
+	private int currentPassengers;
 
 	/**
 	 * Distance per time unit.
@@ -16,11 +18,29 @@ public class Train extends Thread {
 	private int speed;
 	private boolean hasArrived = false;
 
-	public Train(Line line, Canton startCanton, int speed) {
+	public Train(Line line, Canton startCanton, int speed, int currentPassenger , int maxPassenger) {
 		this.line = line;
 		currentCanton = startCanton;
 		currentCanton.enter(this);
 		this.speed = speed;
+		this.currentCanton = currentCanton;
+		this.maxPassenger = maxPassenger;
+	}
+
+	public int getMaxPassenger() {
+		return maxPassenger;
+	}
+
+	public void setMaxPassenger(int maxPassenger) {
+		this.maxPassenger = maxPassenger;
+	}
+
+	public int getCurrentPassengers() {
+		return currentPassengers;
+	}
+
+	public void setCurrentPassengers(int currentPassengers) {
+		this.currentPassengers = currentPassengers;
 	}
 
 	public int getPosition() {
