2aa8bf4046ba538f0cf3e9dffa34d9b27201826e
gb <gb@DESKTOP-8NTSBQO>
2018-02-15 19:11:25 +0100

    Ajout des fonctions liées a la génération des passagers.

--- Entrotrain/src/core/Passenger.java
+++ Entrotrain/src/core/Passenger.java
@@ -1,43 +1,67 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Passenger {
 	private int age;
-	private String sexe;
-	private Station stationOfArrival; 
-	private Station stationOfDeparture;
+	private String sex;
+	private int idArrival; 
+	private int idDeparture;
 	private int satisfaction;
 
 
-public Passenger(int age, String sexe, Station arrival, Station departure, int satisfaction){
-	this.age = age;
-	this.sexe = sexe;
-	this.stationOfArrival = stationOfArrival;
-	this.stationOfDeparture = stationOfDeparture;
-	this.satisfaction = satisfaction;
-}
-
-
-
-	public Station getStationOfArrival() {
-		return stationOfArrival;
-	}
-	public void setStationOfArrival(Station stationOfArrival) {
-		this.stationOfArrival = stationOfArrival;
+	public Passenger(int age, String sex, int idArrival, int idDeparture, int satisfaction){
+		this.age = age;
+		this.sex = sex;
+		this.idArrival = idArrival;
+		this.idDeparture = idDeparture;
+		this.satisfaction = satisfaction;
+	}	
+	public ArrayList<Passenger> generatePassengers(int numberPassenger,int idDeparture, int idTerminus) {
+		ArrayList<Passenger> passengers = new ArrayList<Passenger>();
+		for(int i=0;i<=numberPassenger;i++) {
+			Passenger pass = generatePassenger(idDeparture,idTerminus);
+			passengers.add(pass);
+		}
+		return passengers;
+	}	
+	public Passenger generatePassenger(int idDeparture,int idTerminus){
+		Random rand = new Random();
+		int age = 7 + rand.nextInt(99-7);
+		int idStationArrival = (idDeparture+1)+rand.nextInt(idTerminus-idDeparture+1);
+		ArrayList<String> sexs = new ArrayList<String>();
+		sexs.add("m");
+		sexs.add("s");
+		int index = rand.nextInt(1-0);
+		String sex = sexs.get(index);
+		Passenger pass = new Passenger(age,sex,idStationArrival,idDeparture, 5);
+		return pass;
 	}
 	
-	public Station getStationOfDeparture() {
-		return stationOfDeparture;
+	
+	public int getIdArrival() {
+		return idArrival;
 	}
-	public void setStationOfDeparture(Station stationOfDeparture) {
-		this.stationOfDeparture = stationOfDeparture;
+	public void setIdArrival(int idArrival) {
+		this.idArrival = idArrival;
 	}
-
+	public int getIdDeparture() {
+		return idDeparture;
+	}
+	public void setIdDeparture(int idDeparture) {
+		this.idDeparture = idDeparture;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String getSex() {
+		return sex;
+	}
 	public Passenger update_satisfaction(Passenger pass,int value) {
 		pass.setSatisfaction(pass.getSatisfaction()-value);
 		return pass;
 	}
-
 	public int getSatisfaction() {
 		return satisfaction;
 	}
